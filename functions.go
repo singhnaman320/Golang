77dd61a838ee965d0b2cfc0264db4cc544b83c57
naman kumar singh <singhnaman320@gmail.com>
2023-03-26 14:59:48 +0530

    Functions with Golang

--- functions.go
+++ functions.go
@@ -23,7 +23,7 @@ import "fmt"
 
 func rectanglePerimeter(len, bre int) int {
 
-	perimeter := 2*(len + bre)
+	perimeter := 2 * (len + bre)
 
 	return perimeter
 }
@@ -36,6 +36,38 @@ func rectangleArea(length, breadth int) {
 	fmt.Println("Area of rectangle is:", area)
 }
 
+// [] Function Arguments
+
+// In Go language, the parameters passed to a function are called actual parameters, whereas the parameters received by a function are called
+// formal parameters.
+// Note: By default Go language use call by value method to pass arguments in function.
+// Go language supports two ways to pass arguments to your function:
+
+// Call by value: In this parameter passing method, values of actual parameters are copied to function’s formal parameters and the two types of
+// parameters are stored in different memory locations. So any changes made inside functions are not reflected in actual parameters of the caller.
+
+func swapByValue(value1, value2 int) int {
+
+	var num int
+	num = value1
+	value1 = value2
+	value2 = num
+
+	return num
+}
+
+// Call by reference: Both the actual and formal parameters refer to the same locations, so any changes made inside the function are actually
+// reflected in actual parameters of the caller.
+
+func swapByReference(val1, val2 *int) int {
+	var store int
+	store = *val1
+	*val1 = *val2
+	*val2 = store
+
+	return store
+}
+
 // =--=-=--==-=-=-=-=-=--==- main =-=-==-=-=-=-==-=-=--=-=-=-=-=-=-=-=-=-
 
 func main() {
@@ -43,6 +75,21 @@ func main() {
 	rectangleArea(6, 5)
 
 	fmt.Println("Perimeter of rectangle is:", rectanglePerimeter(6, 5))
+
+	// call by values
+	var v1 int = 10
+	var v2 int = 20
+	fmt.Printf("\n v1 = %d and v2 = %d", v1, v2)
+
+	swapByValue(v1, v2)
+	fmt.Printf("\n v1 = %d and v2 = %d", v1, v2)
+
+	// call by reference
+	fmt.Println("By Reference")
+	fmt.Printf("\nv1 = %d and v2 = %d", v1, v2)
+
+	swapByReference(&v1, &v2)
+	fmt.Printf("\np = %d and q = %d", v1, v2)
 }
 
 // =-=-=-=-=-==-=--==-=---=-=-=-=-=-=--=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=
