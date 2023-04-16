076ac6f69149fb3a2d1f50e7e7ca8a6edcd48951
naman kumar singh <singhnaman320@gmail.com>
2023-04-16 23:19:26 +0530

    Golang map

--- map/mapOne.go
+++ map/mapOne.go
@@ -5,6 +5,24 @@ import "fmt"
 // Golang Maps is a collection of unordered pairs of key-value. It is widely used because it provides fast lookups and values that can retrieve, 
 // update or delete with the help of keys.
 
+// How to creating and initializing Maps?
+
+// In Go language, maps can create and initialize using two different ways: 
+
+// 1. Simple: In this method, you can create and initialize a map without the use of make() function:
+
+// Creating Map: You can simply create a map using the given syntax:
+
+// An Empty map
+// map[Key_Type]Value_Type{}
+
+// Map with key-value pair
+// map[Key_Type]Value_Type{key1: value1, ..., keyN: valueN}
+
+// Example: 
+
+// var mymap map[int]string
+
 func main(){
 
 	
