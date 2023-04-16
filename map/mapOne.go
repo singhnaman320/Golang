f885e919440d5f032f243ee994cfb95ca1f72dca
naman kumar singh <singhnaman320@gmail.com>
2023-04-16 23:13:11 +0530

    Interview prep

---
+++ map/mapOne.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// Golang Maps is a collection of unordered pairs of key-value. It is widely used because it provides fast lookups and values that can retrieve, 
+// update or delete with the help of keys.
+
+func main(){
+
+	
+}
