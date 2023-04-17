acfacd549d139ceaf81c497c625527261e3fd615
naman kumar singh <singhnaman320@gmail.com>
2023-04-17 12:54:22 +0530

    map with Golang

--- map/mapTwo.go
+++ map/mapTwo.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
-	
+// [] Using make function:
+
+// You can also create a map using make() function. This function is an inbuilt function and in this method, you just need to pass the type
+// of the map and it will return an initialized map.
+
+// Syntax:
+
+// make(map[Key_Type]Value_Type, initial_Capacity)
+// make(map[Key_Type]Value_Type)
+
+func main() {
+
 }
