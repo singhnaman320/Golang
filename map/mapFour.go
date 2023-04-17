6056d08f33025956ef6261f6c7568b90d9f25013
naman kumar singh <singhnaman320@gmail.com>
2023-04-17 13:31:12 +0530

    Add new key value pairs to map(Golang)

--- map/mapFour.go
+++ map/mapFour.go
@@ -12,4 +12,27 @@ import "fmt"
 
 func main() {
 
+	// Creating and initializing a map
+	myMap := map[int]string{
+
+		10: "Naman",
+		12: "Kaushik",
+		30: "Vishal",
+		14: "Nishant",
+		9:  "Prashant",
+	}
+
+	fmt.Println("Original map:", myMap)
+
+	// Adding new key-value pairs in the map
+	myMap[60] = "Rishabh"
+	myMap[22] = "Abhinav"
+
+	fmt.Println("\nMap after adding new key-value pair:\n", myMap)
+
+	// Updating values of the map
+	myMap[30] = "Tushar"
+	myMap[9] = "Yashika"
+
+	fmt.Println("\nMap after updating values of the map:\n", myMap)
 }
