e61e35bfef9f2032806faf8cffc1c1c0ef0cb530
naman kumar singh <singhnaman320@gmail.com>
2023-04-17 13:16:18 +0530

    Iteration over map in Golang

--- map/mapThree.go
+++ map/mapThree.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
-// [] Iteration over a map: 
+// [] Iteration over a map:
 
 // You can iterate a map using the range for loop. The value of this loop may vary because the map is an unordered collection.
 
-func main(){
+func main() {
 
+	mapIteration := map[int]string{
+
+		10: "Naman",
+		12: "Kaushik",
+		30: "Vishal",
+		14: "Nishant",
+		9:  "Prashant",
+	}
+
+	for roll, studentName := range mapIteration {
+
+		fmt.Println(roll, studentName)
+	}
 }
