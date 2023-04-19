39feaccc78d475e24f3df2cfa968639da42b57e9
naman kumar singh <singhnaman320@gmail.com>
2023-04-19 19:48:06 +0530

    Date and Time with Golang

---
+++ DateAndTime/dtOne.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// In Go language, time packages supplies functionality for determining as well as viewing time. The Date() function in Go language is used to
+// find Date the Time which is equivalent to yyyy-mm-dd hh:mm:ss + nsec nanoseconds in the proper time zone in the stated location. Moreover,
+// this function is defined under the time package. Here, you need to import the “time” package in order to use these functions.
+
+// Syntax:
+
+// func Date(year int, month Month, day, hour, min, sec, nsec int, loc *Location) Time
+
+// Here, “loc” is pointed to the location.
+
+// Return Value: It returns a time that is proper in one of the two associated zones in the conversion, but it won’t guarantee that which one
+// is returned. And it returns panics if the given “loc” is nil.
+
+// Note: Here, the value of the month, day hour, min, sec and nsec can be more than the normal ranges but they will be automatically normalized
+// during the conversion. For example, if the date is April 34 then it is converted to May 1.
+
+func main(){
+
+}
--- DateAndTime/dtThree.go
+++ DateAndTime/dtThree.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	fmt.Println("Learn date and time with Golang")
-	parsentTime := time.Now()
+	gienTime := time.Now()
 
-	fmt.Println("\nTime one: ",parsentTime)
-	fmt.Println("\nTime two: ",parsentTime.Format("01-02-2006 15:04:05 Monday"))
+	fmt.Println("\nTime one: ", gienTime)
+	fmt.Println("\nTime two: ", gienTime.Format("01-02-2006 15:04:05 Monday"))
+
 	cratedDate := time.Date(2020, time.August, 10, 23, 23, 0, 0, time.UTC)
-	fmt.Println("\nDate one",cratedDate)
-	fmt.Println("\nDate Two",cratedDate.Format("01-02-2016 15:04:05 Monday"))
+
+	fmt.Println("\nDate one", cratedDate)
+	fmt.Println("\nDate Two", cratedDate.Format("01-02-2016 15:04:05 Monday"))
 }
