b739d90847a3cc91b0006dd96abcaa8363909bdd
naman kumar singh <singhnaman320@gmail.com>
2023-04-19 13:43:40 +0530

    Map with Golang

---
+++ map/mapEight.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Modifying map: 
+
+// As we know that maps are of reference type. So, when we assign an existing map to a new variable, both the maps still refer to the same 
+// underlying data structure. So, when we update one map it will reflect in another map.
+
+func main(){
+
+
+
+
+}
