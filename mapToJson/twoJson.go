5f1e91b3e6755571a0f6bcd70858a8d5578d5c7d
naman kumar singh <singhnaman320@gmail.com>
2023-04-19 16:59:20 +0530

    Map with Golang

--- mapToJson/twoJson.go
+++ mapToJson/twoJson.go
@@ -5,6 +5,29 @@ import (
 	"fmt"
 )
 
-func main(){
-	
+// Example where we convert a map to a JSON where we have a struct for the value in the map. Below is the code for that
+
+type employee struct {
+	Name string
 }
+
+func main() {
+
+	myMap := make(map[int]employee)
+
+	myMap[1] = employee{Name: "Naman"}
+
+	value, err := json.Marshal(myMap)
+
+	if err != nil {
+
+		fmt.Printf("Error: %s", err.Error())
+
+	} else {
+
+		fmt.Println(string(value))
+	}
+
+}
+
+// Output: {"1":{"Name":"Naman"}}
