fc4960d7271e17cdc03d56404270d76e102d7173
naman kumar singh <singhnaman320@gmail.com>
2023-04-19 13:40:10 +0530

    Delete key in map with Golang

--- map/mapSeven.go
+++ map/mapSeven.go
@@ -2,16 +2,33 @@ package main
 
 import "fmt"
 
-// Delete key from the map: 
+// Delete key from the map:
 
-// In maps, you are allowed to delete the key present in the map using the delete() function. It is inbuilt function and does not return any 
-// value and does not do anything if the key does not present in the given map. In this function, you just simply pass the map and key which 
+// In maps, you are allowed to delete the key present in the map using the delete() function. It is inbuilt function and does not return any
+// value and does not do anything if the key does not present in the given map. In this function, you just simply pass the map and key which
 // you want to delete from the map.
 
-// Syntax: 
+// Syntax:
 
 // delete(map_name, key)
 
-func main(){
+func main() {
 
+	myMap := map[int]string{
+
+		10: "Naman",
+		12: "Kaushik",
+		30: "Vishal",
+		14: "Nishant",
+		9:  "Prashant",
+	}
+
+	fmt.Println("original map is: ", myMap)
+
+	// Deleting keys using delete function
+
+	delete(myMap, 9)
+	delete(myMap, 30)
+
+	fmt.Println("Map after deletion is: ", myMap)
 }
