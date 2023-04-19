0d454c8a4e7b792d60ae5860e9b4c914f258520b
naman kumar singh <singhnaman320@gmail.com>
2023-04-19 15:55:43 +0530

    Map to Json with Golang

--- mapToJson/oneJson.go
+++ mapToJson/oneJson.go
@@ -17,7 +17,11 @@ func main() {
 	thisMap[1] = "Naman"
 	thisMap[2] = "Kaushik"
 
+	// In the above code, we are using json.Marshal function to convert the map to JSON. The map has an integer value for the key.
+
 	value, err := json.Marshal(thisMap)
+
+	// While after converting, the resultant JSON as a string value for the key
 
 	if err != nil {
 
@@ -28,3 +32,7 @@ func main() {
 		fmt.Println(string(value))
 	}
 }
+
+// Output:
+
+// {"1":"Naman","2":"Kaushik"}
---
+++ mapToJson/twoJson.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main(){
+	
+}
