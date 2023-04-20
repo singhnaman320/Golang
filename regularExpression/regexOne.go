f57d2f759a80a6df42a7b6e58a3f4b7a90a12c3c
naman kumar singh <singhnaman320@gmail.com>
2023-04-20 13:17:13 +0530

    regexp with Golang

---
+++ regularExpression/regexOne.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"regexp"
+	"fmt"
+)
+
+// A Regular Expression (or RegEx) is a special sequence of characters that defines a search pattern that is used for matching specific text. 
+// In Golang, there’s a built-in package for regular expressions, called the regexp package which contains all list of actions like filtering, 
+// replacing, validating, or extracting. It uses the RE2 syntax standards. The MatchString() function reports whether the string passed as a 
+// parameter contains any match of the regular expression pattern.
+
+// Syntax:
+
+// func MatchString(pattern string, s string)
+
+// Returns: matched bool, err error
+
+func main(){
+
+
+}
